7ef7e3b4e2b52eb54076f71cc8c935d34fae07ff
Nyall Dawson <nyall.dawson@gmail.com>
2024-06-05 13:02:34 +1000

    Rename existing fid columns to old_fid when export to gpkg fails

--- felt/core/layer_exporter.py
+++ felt/core/layer_exporter.py
@@ -333,7 +333,9 @@ class LayerExporter(QObject):
             self,
             layer: QgsVectorLayer,
             conversion_context: ConversionContext,
-            feedback: Optional[QgsFeedback] = None) -> LayerExportDetails:
+            feedback: Optional[QgsFeedback] = None,
+            force_rewrite_fid: bool = False
+    ) -> LayerExportDetails:
         """
         Exports a vector layer into a format acceptable for Felt
         """
@@ -349,7 +351,9 @@ class LayerExporter(QObject):
             self.transform_context
         )
         writer_options.feedback = feedback
-        writer_options.forceMulti = True
+        writer_options.forceMulti = QgsWkbTypes.geometryType(
+            layer.wkbType()) in (QgsWkbTypes.LineGeometry,
+                                 QgsWkbTypes.PolygonGeometry)
         writer_options.overrideGeometryType = QgsWkbTypes.dropM(
             QgsWkbTypes.dropZ(layer.wkbType())
         )
@@ -366,13 +370,13 @@ class LayerExporter(QObject):
         writer_options.attributes = fields.allAttributesList()
         if fid_index >= 0:
             fid_type = fields.field(fid_index).type()
-            if fid_type not in (QVariant.Int,
-                                QVariant.UInt,
-                                QVariant.LongLong,
-                                QVariant.ULongLong):
-                writer_options.attributes = [a for a in
-                                             writer_options.attributes if
-                                             a != fid_index]
+            if force_rewrite_fid or fid_type not in (QVariant.Int,
+                                                     QVariant.UInt,
+                                                     QVariant.LongLong,
+                                                     QVariant.ULongLong):
+                writer_options.attributesExportNames = [
+                    f.name() if f.name().lower() != 'fid' else 'old_fid'
+                    for f in fields]
 
         # pylint: disable=unused-variable
         res, error_message, new_filename, new_layer_name = \
@@ -384,7 +388,14 @@ class LayerExporter(QObject):
             )
         # pylint: enable=unused-variable
 
-        if res not in (QgsVectorFileWriter.WriterError.NoError,
+        if (not force_rewrite_fid and
+                res == QgsVectorFileWriter.WriterError.ErrFeatureWriteFailed):
+            # could not write attributes -- possibly eg due to duplicate
+            # FIDs. Let's try with renaming FID
+            return self.export_vector_layer(
+                layer, conversion_context, feedback, True
+            )
+        elif res not in (QgsVectorFileWriter.WriterError.NoError,
                        QgsVectorFileWriter.WriterError.Canceled):
             Logger.instance().log_error_json(
                 {
