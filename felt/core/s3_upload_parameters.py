2714662cb7c7996d72167371674a8f08a7097c90
Chris Loer <chris@felt.com>
2024-10-16 12:12:15 -0700

    Fix line length requirements

--- felt/core/s3_upload_parameters.py
+++ felt/core/s3_upload_parameters.py
@@ -8,7 +8,8 @@ from typing import Dict
 @dataclass
 class S3UploadParameters:
     """
-    Encapsulates parameters for uploading to S3, including all presigned attributes
+    Encapsulates parameters for uploading to S3, including all presigned
+    attributes
     """
 
     url: str
@@ -18,14 +19,16 @@ class S3UploadParameters:
 
     def to_form_fields(self) -> Dict:
         """
-        Returns all form fields including the presigned attributes required for the upload
+        Returns all form fields including the presigned attributes required
+        for the upload
         """
         return {**self._presigned_attributes}
 
     @staticmethod
     def from_json(res: Dict[str, str]) -> 'S3UploadParameters':
         """
-        Creates upload parameters from a JSON response, capturing all presigned attributes
+        Creates upload parameters from a JSON response, capturing all
+        presigned attributes
         """
         return S3UploadParameters(
             url=res.get('url'),
